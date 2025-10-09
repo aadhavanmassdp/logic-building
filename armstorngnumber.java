73f88cb0ddfd9e32b1e1ecb01e0e4035183e2d77
aadhavan raja <68787357+aadhavanmassdp@users.noreply.github.com>
2025-10-09 10:20:07 +0530

    Create armstorngnumber.java

---
+++ armstorngnumber.java
@@ -0,0 +1,17 @@
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        int num = 153;
+        int sum = 0, temp = num;
+
+        while (temp != 0) {
+            int digit = temp % 10;
+            sum += digit * digit * digit;
+            temp /= 10;
+        }
+
+        if (num == sum)
+            System.out.println(num + " is an Armstrong number");
+        else
+            System.out.println(num + " is not an Armstrong number");
+    }
+}
