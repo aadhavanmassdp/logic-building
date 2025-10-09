f83d1aed030208b2baf806bc65017fab2c299d43
aadhavan raja <68787357+aadhavanmassdp@users.noreply.github.com>
2025-10-09 10:21:18 +0530

    Create countdigit.java

---
+++ countdigit.java
@@ -0,0 +1,13 @@
+public class CountDigits {
+    public static void main(String[] args) {
+        int num = 12345;
+        int count = 0;
+
+        while (num != 0) {
+            num /= 10;   // Remove the last digit
+            count++;     // Increase count
+        }
+
+        System.out.println("Total digits: " + count);
+    }
+}
