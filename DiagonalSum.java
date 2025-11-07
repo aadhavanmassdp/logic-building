74745e668f737e4a1be48a4117c1eb52d07dbf7a
aadhavan raja <68787357+aadhavanmassdp@users.noreply.github.com>
2025-11-07 11:38:20 +0530

    Create  DiagonalSum.java

---
+++ DiagonalSum.java
@@ -0,0 +1,24 @@
+public class DiagonalSum {
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        int n = matrix.length;
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += matrix[i][i];          // primary diagonal
+            sum += matrix[i][n - 1 - i];  // secondary diagonal
+        }
+
+        if (n % 2 != 0) {
+            sum -= matrix[n / 2][n / 2];  // avoid double-counting center
+        }
+
+        System.out.println("Sum of diagonal elements: " + sum);
+    }
+}
+
