d3ce87644b4fb6c22b08bcdbc8672294c4d4e6b4
aadhavan raja <68787357+aadhavanmassdp@users.noreply.github.com>
2025-11-08 10:23:54 +0530

    Create MatrixMultiplication.java

---
+++ MatrixMultiplication.java
@@ -0,0 +1,38 @@
+public class MatrixMultiplication {
+    public static void main(String[] args) {
+        int[][] a = {
+            {1, 2, 3},
+            {4, 5, 6}
+        };
+
+        int[][] b = {
+            {7, 8},
+            {9, 10},
+            {11, 12}
+        };
+
+        int rowsA = a.length;
+        int colsA = a[0].length;
+        int colsB = b[0].length;
+
+        int[][] result = new int[rowsA][colsB];
+
+        // Multiplication logic
+        for (int i = 0; i < rowsA; i++) {
+            for (int j = 0; j < colsB; j++) {
+                for (int k = 0; k < colsA; k++) {
+                    result[i][j] += a[i][k] * b[k][j];
+                }
+            }
+        }
+
+        // Display result
+        System.out.println("Product of two matrices:");
+        for (int[] row : result) {
+            for (int val : row) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+        }
+    }
+}
