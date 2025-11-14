a153a993fb0d95e5b27995a1216f4870a20af15e
aadhavan raja <68787357+aadhavanmassdp@users.noreply.github.com>
2025-11-14 10:21:17 +0530

    Create  SwapWithoutTemp.java

---
+++ SwapWithoutTemp.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class SwapWithoutTemp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter b: ");
+        int b = sc.nextInt();
+
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+
+        System.out.println("After swapping:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
+
+
+Out put :
+
+Enter a: 5
+Enter b: 9
+After swapping:
+a = 9
+b = 5
