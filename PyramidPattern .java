b505bab32ccbeea79d5eab409e9863f7ad490a66
aadhavan raja <68787357+aadhavanmassdp@users.noreply.github.com>
2025-11-15 10:02:42 +0530

    Create  PyramidPattern .java

---
+++ PyramidPattern .java
@@ -0,0 +1,27 @@
+public class PyramidPattern {
+    public static void main(String[] args) {
+        int n = 5; // height
+
+        for (int i = 1; i <= n; i++) {
+            // print spaces
+            for (int s = 1; s <= n - i; s++) {
+                System.out.print(" ");
+            }
+
+            // print stars
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
+
+
+output
+    *
+   ***
+  *****
+ *******
+*********
