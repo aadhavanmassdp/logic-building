67e0c0c5d6a85b9bf47fa235fd9d6ec3f51c10c7
aadhavan raja <68787357+aadhavanmassdp@users.noreply.github.com>
2025-11-15 10:00:50 +0530

    Create TrianglePattern.java

---
+++ TrianglePattern.java
@@ -0,0 +1,23 @@
+
+
+public class TrianglePattern {
+    public static void main(String[] args) {
+        int n = 5; // height of the triangle
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+Output:
+
+*
+**
+***
+****
+*****
